df5542d22c7fb5e326d287a34e601ef79a0b9082
rmazzine <mazzine.r@gmail.com>
2021-02-13 20:07:51 +0100

    Detect stereo audio and convert to mono
    The transcriber works better with mono channel, therefore, this commit detects if the audio is stereo and, if yes, convert it to mono.

--- flaskapp/support/vosk_transcriber.py
+++ flaskapp/support/vosk_transcriber.py
@@ -1,4 +1,5 @@
 import os
+import audioop
 
 from vosk import Model, KaldiRecognizer
 import wave
@@ -9,6 +10,21 @@ model = Model("model")
 def transcribe_wav(filepath):
 
     wf = wave.open(filepath, 'rb')
+
+    n_channels = wf.getnchannels()
+
+    # If not mono, convert it to mono
+    if n_channels != 1:
+        mono_filename = f'{filepath}.monofile.wav'
+        mono = wave.open(mono_filename, 'wb')
+        mono.setparams(wf.getparams())
+        mono.setnchannels(1)
+        mono.writeframes(audioop.tomono(wf.readframes(float('inf')), wf.getsampwidth(), 1, 1))
+        mono.close()
+
+        wf = wave.open(mono_filename, 'rb')
+
+        os.remove(mono_filename)
 
     rec = KaldiRecognizer(model, wf.getframerate())
 
